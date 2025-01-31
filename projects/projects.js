16942a6c83e843d81ab6dbb22f2a451b854fea8d
Hargen Z. <yoz018@ucsd.edu>
2025-01-30 21:28:54 -0800

    Minor update.

--- projects/projects.js
+++ projects/projects.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     const projectsContainer = document.querySelector('.projects');
 
     if (projectsContainer) {
-        const projects = await fetchJSON('../lib/projects.json');
+        const projects = await fetchJSON("./lib/projects.json");
         renderProjects(projects, projectsContainer, 'h2');
     }
 })
